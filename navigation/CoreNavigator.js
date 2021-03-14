b71c03e82a6d981e52fa9f6d429f2c9163775637
kaveenhyacinth <alwiskaveen@gmail.com>
2021-03-14 15:27:43 +0530

    Modify core navigator to fetch user data

--- navigation/CoreNavigator.js
+++ navigation/CoreNavigator.js
@@ -1,21 +1,18 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons, FontAwesome5 } from "@expo/vector-icons";
+import { useDispatch } from "react-redux";
+import { getNomads } from "../store/actions/storeNomad";
+import { Fetch } from "../services/deviceStorage";
 import Colors from "../theme/Colors";
 import ExploreNavigation from "./ExploreNavigation";
 import TribeNavigation from "./TribeNavigation";
 import ProfileNavigation from "./ProfileStackNavigation";
 import HomeNavigation from "./HomeNavigation";
 import InboxScreen from "../screens/core/InboxScreen";
+import LoadingScreen from "../screens/extra/LoadingScreen";
 
 const Tab = createBottomTabNavigator();
-
-const renderFaIcons = (tabInfo) => (name, size) => (
-  <FontAwesome5 name={name} size={size} color={tabInfo.color} />
-);
-const renderIcIcons = (tabInfo) => (name, size) => (
-  <Ionicons name={name} size={size} color={tabInfo.color} />
-);
 
 const defaultTabOptions = {
   activeTintColor: Colors.primary,
@@ -23,6 +20,45 @@ const defaultTabOptions = {
 };
 
 const CoreNavigator = (props) => {
+  const [loading, setLoading] = useState(false);
+  const dispatch = useDispatch();
+  useEffect(() => {
+    const handleLoadNomad = async () => {
+      try {
+        setLoading(true);
+        const nomadId = await Fetch("nomadId");
+        await getNomads(nomadId)(dispatch);
+      } catch (error) {
+        Alert.alert(
+          "Oh My trod!",
+          error.message ?? "Something went wrong. Please try again later",
+          [
+            {
+              text: "I Will",
+              style: "destructive",
+            },
+          ],
+          { cancelable: false }
+        );
+        console.log("Error Happen", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+    handleLoadNomad();
+  }, []);
+
+  const renderFaIcons = (tabInfo) => (name, size) => (
+    <FontAwesome5 name={name} size={size} color={tabInfo.color} />
+  );
+  const renderIcIcons = (tabInfo) => (name, size) => (
+    <Ionicons name={name} size={size} color={tabInfo.color} />
+  );
+
+  if (loading) {
+    return <LoadingScreen />;
+  }
+
   return (
     <Tab.Navigator
       tabBarOptions={{
