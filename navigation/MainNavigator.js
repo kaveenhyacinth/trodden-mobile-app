0defd3d1c6c759bd9fbaa967d5fb02a43efe2f06
kaveenhyacinth <alwiskaveen@gmail.com>
2021-03-14 15:27:04 +0530

    Update main navigator useEffect async call

--- navigation/MainNavigator.js
+++ navigation/MainNavigator.js
@@ -20,50 +20,51 @@ const MainNavigator = (props) => {
   const dispatch = useDispatch();
 
   useEffect(() => {
+    // Check storage ref token availability
+    const checkIsSignedIn = async () => {
+      setLoading(true);
+      try {
+        // Fetch token form localstorage
+        const refToken = await Fetch("refToken");
+        if (!refToken) return setIsSignedIn(false);
+
+        // Request for new tokens
+        const refreshTokenBody = {
+          refreshToken: refToken,
+        };
+        const response = await api.refreshToken(refreshTokenBody);
+
+        if (!response.data.result) throw new Error("Something went wrong!");
+
+        const newSignToken = response.data.result.signToken;
+        const newRefToken = response.data.result.refToken;
+        const nomadId = response.data.result.id;
+
+        // Update state and localstorage with new tokens
+        handleUpdateTokens(newSignToken, newRefToken);
+        await Save("signToken", newSignToken);
+        await Save("refToken", newRefToken);
+        await Save("nomadId", nomadId);
+
+        return setIsSignedIn(true);
+      } catch (error) {
+        Alert.alert(
+          "Oh My Trod!",
+          "Something went wrong! Please check your internet connection and try again...",
+          [
+            {
+              text: "I Will",
+              style: "destructive",
+            },
+          ],
+          { cancelable: false }
+        );
+      } finally {
+        setLoading(false);
+      }
+    };
     checkIsSignedIn();
   }, []);
-
-  // Check storage ref token availability
-  const checkIsSignedIn = async () => {
-    setLoading(true);
-    try {
-      // Fetch token form localstorage
-      const refToken = await Fetch("refToken");
-      if (!refToken) return setIsSignedIn(false);
-
-      // Request for new tokens
-      const refreshTokenBody = {
-        refreshToken: refToken,
-      };
-      const response = await api.refreshToken(refreshTokenBody);
-
-      if (!response.data.result) throw new Error("Something went wrong!");
-
-      const newSignToken = response.data.result.signToken;
-      const newRefToken = response.data.result.refToken;
-
-      // Update state and localstorage with new tokens
-      handleUpdateTokens(newSignToken, newRefToken);
-      await Save("signToken", newSignToken);
-      await Save("refToken", newRefToken);
-
-      return setIsSignedIn(true);
-    } catch (error) {
-      Alert.alert(
-        "Oh My Trod!",
-        "Something went wrong! Please check your internet connection and try again...",
-        [
-          {
-            text: "I Will",
-            style: "destructive",
-          },
-        ],
-        { cancelable: false }
-      );
-    } finally {
-      setLoading(false);
-    }
-  };
 
   const handleUpdateTokens = (signToken, refToken) => {
     dispatch(storeToken(signToken, refToken));
