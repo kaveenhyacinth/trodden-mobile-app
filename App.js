4de1651d33149058317671047aaf4e79a2a04f4c
kaveenhyacinth <alwiskaveen@gmail.com>
2021-04-11 07:27:44 +0530

    Lift check is user to app.js

--- App.js
+++ App.js
@@ -5,6 +5,11 @@ import React, { useState } from "react";
 import * as Font from "expo-font";
 import { NavigationContainer } from "@react-navigation/native";
 import { Provider } from "react-redux";
+import { Alert } from "react-native";
+import { useDispatch } from "react-redux";
+import { Save, Fetch } from "./helpers/deviceStorageHandler";
+import { storeTokens } from "./redux";
+import api from "./api";
 import MainNavigator from "./routes/MainNavigator";
 import store from "./redux/store";
 // import { useScreens } from "react-native-screens";
@@ -24,6 +29,48 @@ const fetchFonts = () => {
 
 export default function App() {
   const [dataLoaded, setDataLoaded] = useState(false);
+  const [isUser, setIsUser] = useState(false);
+  const dispatch = useDispatch();
+
+  const handleUpdateTokens = (signToken, refToken) => {
+    dispatch(storeTokens(signToken, refToken));
+  };
+
+  const handleCheckIsUser = async () => {
+    try {
+      // Fetch token form localstorage
+      const refToken = await Fetch("refToken");
+      if (!refToken) return isUser(false);
+
+      // Request for new tokens
+      const refreshTokenBody = {
+        refreshToken: refToken,
+      };
+      const response = await api.post.refreshToken(refreshTokenBody);
+
+      if (!response.data.result) throw new Error("Something went wrong!");
+
+      const newSignToken = response.data.result.signToken;
+      const newRefToken = response.data.result.refToken;
+      const nomadId = response.data.result.id;
+
+      // Update state and localstorage with new tokens
+      handleUpdateTokens(newSignToken, newRefToken);
+      await Save("signToken", newSignToken);
+      await Save("refToken", newRefToken);
+      await Save("nomadId", nomadId);
+
+      return isUser(true);
+    } catch (error) {
+      console.error("Error Happen in sign in", error);
+      setIsUser(false);
+    }
+  };
+
+  const handleAppLoading = () => {
+    fetchFonts();
+    // handleCheckIsUser();
+  };
 
   if (!dataLoaded) {
     return (
@@ -38,7 +85,7 @@ export default function App() {
   return (
     <Provider store={store}>
       <NavigationContainer>
-        <MainNavigator />
+        <MainNavigator isUser={isUser} />
         <StatusBar style="auto" />
       </NavigationContainer>
     </Provider>
--- routes/MainNavigator.js
+++ routes/MainNavigator.js
@@ -1,10 +1,5 @@
 import React, { useState, useEffect } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
-import { Alert } from "react-native";
-import { useDispatch } from "react-redux";
-import { Save, Fetch } from "../helpers/deviceStorageHandler";
-import { storeTokens } from "../redux";
-import api from "../api";
 import AuthNavigator from "./AuthNavigator";
 import PostAuthNavigator from "./PostAuthNavigator";
 import CoreNavigator from "./CoreNavigator";
@@ -12,70 +7,8 @@ import LoadingScreen from "../screens/info/LoadingScreen";
 
 const Stack = createStackNavigator();
 
-const MainNavigator = (props) => {
-  const [loading, setLoading] = useState(true);
-  const [isSignedIn, setIsSignedIn] = useState(false);
-
-  // Update tokens store with new tokens
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    // Check storage ref token availability
-    const checkIsSignedIn = async () => {
-      setLoading(true);
-      try {
-        // Fetch token form localstorage
-        const refToken = await Fetch("refToken");
-        if (!refToken) return setIsSignedn(false);
-
-        // Request for new tokens
-        const refreshTokenBody = {
-          refreshToken: refToken,
-        };
-        const response = await api.post.refreshToken(refreshTokenBody);
-
-        if (!response.data.result) throw new Error("Something went wrong!");
-
-        const newSignToken = response.data.result.signToken;
-        const newRefToken = response.data.result.refToken;
-        const nomadId = response.data.result.id;
-
-        // Update state and localstorage with new tokens
-        handleUpdateTokens(newSignToken, newRefToken);
-        await Save("signToken", newSignToken);
-        await Save("refToken", newRefToken);
-        await Save("nomadId", nomadId);
-
-        return setIsSignedIn(true);
-      } catch (error) {
-        Alert.alert(
-          "Oh My Trod!",
-          "Something went wrong! Please check your internet connection and try again...",
-          [
-            {
-              text: "I Will",
-              style: "destructive",
-            },
-          ],
-          { cancelable: false }
-        );
-      } finally {
-        setLoading(false);
-      }
-    };
-    checkIsSignedIn();
-  }, []);
-
-  const handleUpdateTokens = (signToken, refToken) => {
-    dispatch(storeTokens(signToken, refToken));
-  };
-
-  if (loading) {
-    return <LoadingScreen />;
-  }
-
-  // Navigate accordign to auth state
-  return isSignedIn ? (
+const MainNavigator = ({ isUser }) => {
+  return isUser ? (
     <Stack.Navigator headerMode="none">
       <Stack.Screen name="core" component={CoreNavigator} />
     </Stack.Navigator>
