73b3c969c513e8e724525cd92fe136c5a93de374
kaveenhyacinth <alwiskaveen@gmail.com>
2021-05-17 17:16:01 +0530

    remove unwanted lookups

---
+++ redux/lookups/nomad/lookupTypes.js
@@ -0,0 +1,4 @@
+export const FETCH_LN_REQUEST = "FETCH_LN_REQUEST";
+export const FETCH_LN_SUCCESS = "FETCH_LN_SUCCESS";
+export const FETCH_LN_FAILURE = "FETCH_LN_FAILURE";
+export const RESET_LN = "RESET_LN";
